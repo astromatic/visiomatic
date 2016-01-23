0f0b1b07d205f616f1c951aee6c6ecb5eae1c854
Emmanuel Bertin <bertin@iap.fr>
2016-01-23 17:55:33 +0100

    Trying new trick to override  RTD CSS

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -147,6 +147,10 @@ html_title = "<project> v<release>"
 html_static_path = ['theme']
 html_style = 'css/custom.css'
 
+# Trick for having ReadTheDocs read the theme overrides
+def setup(app):
+    app.add_stylesheet("css/custom.css")
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
