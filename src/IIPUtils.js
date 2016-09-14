7e9ea5e8fe4eab8df30b51be093e872b7a05c546
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-09-14 12:38:23 -0300

    Closed #10 - Enable credentials in Ajax requests

--- src/IIPUtils.js
+++ src/IIPUtils.js
@@ -41,6 +41,7 @@ L.IIPUtils = {
 			};
 		}
 		httpRequest.open('GET', url);
+		httpRequest.withCredentials = true;
 		httpRequest.onreadystatechange = function () {
 			action(context, httpRequest);
 		};
