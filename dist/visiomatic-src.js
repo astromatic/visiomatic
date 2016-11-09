b4d426e16079dbf287d3bcbe5443925a89dc7550
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-11-09 15:45:17 -0200

    Added parameter credentials to TileLayer that allows requests using WithCredentials, required for password protected images.

--- dist/visiomatic-src.js
+++ dist/visiomatic-src.js
@@ -771,7 +771,13 @@ L.IIPUtils = {
 			};
 		}
 		httpRequest.open('GET', url);
-//		httpRequest.withCredentials = true;
+
+		// Send Credrentials
+		if ((context) && (context.options.credentials)) {
+			httpRequest.withCredentials = true;
+
+		}
+
 		httpRequest.onreadystatechange = function () {
 			action(context, httpRequest);
 		};
@@ -910,7 +916,8 @@ L.TileLayer.IIP = L.TileLayer.extend({
 		channelLabelMatch: '.*',
 		channelUnits: [],
 		minMaxValues: [],
-		defaultChannel: 0
+		defaultChannel: 0,
+		credentials: false
 		/*
 		pane: 'tilePane',
 		opacity: 1,
@@ -948,7 +955,6 @@ L.TileLayer.IIP = L.TileLayer.extend({
 	},
 
 	initialize: function (url, options) {
-
 		this._url = url.replace(/\&.*$/g, '');
 
 		options = L.setOptions(this, options);
@@ -1128,11 +1134,11 @@ L.TileLayer.IIP = L.TileLayer.extend({
 					}
 				}
 
-				// Copy those units that have been provided 
+				// Copy those units that have been provided
 				for (c = 0; c < ninunits; c++) {
 					units[c] = inunits[c];
 				}
-				// Fill out units that are not provided with a default string 
+				// Fill out units that are not provided with a default string
 				for (c = ninunits; c < nchannel; c++) {
 					units[c] = 'ADUs';
 				}
@@ -2293,7 +2299,7 @@ L.SpinBox = L.Evented.extend({
 				}
 			}, this);
 		}
-	
+
 		if (options.disabled) {
 			this.disable();
 		}
@@ -2439,9 +2445,9 @@ L.spinbox = function (parent, options) {
 //           loadMessage    - Message to display while initial tree loads (can be HTML)
 //
 // TERMS OF USE
-// 
+//
 // This plugin is dual-licensed under the GNU General Public License and the MIT License and
-// is copyright 2008 A Beautiful Site, LLC. 
+// is copyright 2008 A Beautiful Site, LLC.
 //
 */
 
@@ -2583,7 +2589,7 @@ L.Map.addInitHook(function () {
 
 
 /*
-# L.Control.ExtraMap adds support for extra synchronized maps 
+# L.Control.ExtraMap adds support for extra synchronized maps
 # (Picture-in-Picture style). Adapted from L.Control.MiniMap by Norkart
 # (original copyright notice reproduced below).
 #
@@ -2678,7 +2684,7 @@ L.Control.ExtraMap = L.Control.extend({
 		});
 
 		this._layer.addTo(this._extraMap);
-	
+
 		// These bools are used to prevent infinite loops of the two maps notifying
 		// each other that they've moved.
 		// this._mainMapMoving = false;
@@ -2918,17 +2924,17 @@ L.Control.ExtraMap = L.Control.extend({
 		return typeof value !== 'undefined';
 	},
 });
-	
+
 L.Map.mergeOptions({
 	extraMapControl: false
 });
-	
+
 L.Map.addInitHook(function () {
 	if (this.options.extraMapControl) {
 		this.extraMapControl = (new L.Control.ExtraMap()).addTo(this);
 	}
 });
-	
+
 L.control.extraMap = function (layer, options) {
 	return new L.Control.ExtraMap(layer, options);
 };
@@ -2983,21 +2989,21 @@ if (typeof require !== 'undefined') {
 			title: 'Toggle full screen mode',
 			forceSeparateButton: false
 		},
-	
+
 		onAdd: function (map) {
 			var className = 'leaflet-control-zoom-fullscreen', container;
-		
+
 			if (map.zoomControl && !this.options.forceSeparateButton) {
 				container = map.zoomControl._container;
 			} else {
 				container = L.DomUtil.create('div', 'leaflet-bar');
 			}
-		
+
 			this._createButton(this.options.title, className, container, this.toogleFullScreen, map);
 
 			return container;
 		},
-	
+
 		_createButton: function (title, className, container, fn, context) {
 			var link = L.DomUtil.create('a', className, container);
 			link.href = '#';
@@ -3045,7 +3051,7 @@ if (typeof require !== 'undefined') {
 				this._isFullscreen = true;
 			}
 		},
-	
+
 		_handleEscKey: function () {
 			if (!fullScreenApi.isFullScreen(this) && !this._exitFired) {
 				this.fire('exitFullscreen');
@@ -3066,7 +3072,7 @@ if (typeof require !== 'undefined') {
 		return new L.Control.FullScreen(options);
 	};
 
-/* 
+/*
 Native FullScreen JavaScript API
 -------------
 Assumes Mozilla naming conventions instead of W3C for now
@@ -3084,7 +3090,7 @@ source : http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugi
 			prefix: ''
 		},
 		browserPrefixes = 'webkit moz o ms khtml'.split(' ');
-	
+
 	// check for native support
 	if (typeof document.exitFullscreen !== 'undefined') {
 		fullScreenApi.supportsFullScreen = true;
@@ -3098,7 +3104,7 @@ source : http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugi
 			}
 		}
 	}
-	
+
 	// update methods to do something useful
 	if (fullScreenApi.supportsFullScreen) {
 		fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
@@ -3159,6 +3165,7 @@ L.Control.IIP = L.Control.extend({
 		collapsed: true,
 		position: 'topleft'
 	},
+
 
 	initialize: function (baseLayers,  options) {
 		L.setOptions(this, options);
@@ -3558,7 +3565,7 @@ L.Control.IIP = L.Control.extend({
 			}, this);
 			inputdiv.appendChild(input);
 		}
-	
+
 		var name = L.DomUtil.create('div', 'leaflet-control-iip-layername', layerItem);
 		name.innerHTML = ' ' + obj.name;
 		name.style.textShadow = '0px 0px 5px ' + obj.layer.nameColor;
@@ -3963,7 +3970,7 @@ L.Control.IIP.Channel = L.Control.IIP.extend({
 		// Create Mode selection control section
 		modebutton = this._createRadioButton(className + '-radio', modeinput, 'mono',
 		  (this._mode === 'mono'), function () {
-			// Save previous settings 
+			// Save previous settings
 			_this.saveSettings(layer, _this._mode);
 
 			// Remove previous dialogs
@@ -3981,7 +3988,7 @@ L.Control.IIP.Channel = L.Control.IIP.extend({
 
 		modebutton = this._createRadioButton(className + '-radio', modeinput, 'color',
 		  (this._mode !== 'mono'), function () {
-			// Save previous settings 
+			// Save previous settings
 			_this.saveSettings(layer, _this._mode);
 			// Remove previous dialogs
 			for (elem = box.lastChild; elem !== modeline; elem = box.lastChild) {
@@ -4048,7 +4055,7 @@ L.Control.IIP.Channel = L.Control.IIP.extend({
 		this._addMinMax(layer, layer.iipChannel, box);
 		layer.redraw();
 	},
- 
+
 	_initColorDialog: function (layer, box) {
 		// Multiple Channels with mixing matrix
 
@@ -4323,7 +4330,7 @@ L.Control.IIP.Doc = L.Control.IIP.extend({
 	_onloadNav: function () {
 		if (true) {
 			// Force all external iframe links to open in new tab/window
-			// from 
+			// from
 			var	as = this._iframe.contentDocument.getElementsByTagName('a');
 			for (var i = 0; i < as.length; i++) {
 				if (L.IIPUtils.isExternal(as[i].href)) {
@@ -5117,7 +5124,7 @@ L.Control.Layers.IIP = L.Control.Layers.extend({
 			L.DomEvent.on(input, 'click', this._onInputClick, this);
 			inputdiv.appendChild(input);
 		}
-		
+
 		var name = L.DomUtil.create('div', 'leaflet-control-layers-name', item);
 		name.innerHTML = ' ' + obj.name;
 		name.style.textShadow = '0px 0px 5px ' + obj.layer.nameColor;
@@ -5461,7 +5468,7 @@ L.control.scale.wcs = function (options) {
 
 
 /*
-# L.Control.Sidebar adds support for responsive side bars 
+# L.Control.Sidebar adds support for responsive side bars
 # Adapted from the leaflet-sidebar plugin by Tobias Bieniek
 # (original copyright notice reproduced below).
 #
@@ -5549,7 +5556,7 @@ L.Control.Sidebar = L.Control.extend({
 		var className = 'leaflet-control-zoom-sidebar',
 				parent = map._controlContainer,
 		    buttonContainer;
-	
+
 		// Create sidebar
 		L.DomUtil.addClass(map._container, 'sidebar-map');
 		parent.insertBefore(this._sidebar, parent.firstChild);
@@ -5565,7 +5572,7 @@ L.Control.Sidebar = L.Control.extend({
 		} else {
 			buttonContainer = L.DomUtil.create('div', 'leaflet-bar');
 		}
-		
+
 		this._toggleButton = this._createButton(this.options.title,
 		  className + (this.options.collapsed ? ' collapsed' : ''), buttonContainer);
 
