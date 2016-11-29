2479afd1a1e0207e333f393cf047aab59dbb0457
Emmanuel Bertin <bertin@iap.fr>
2016-11-29 13:39:47 +0100

    Fixed typos in Glauber's changes

--- src/IIPUtils.js
+++ src/IIPUtils.js
@@ -155,18 +155,18 @@ L.IIPUtils = {
 
 	// returns the value of a specified cookie (from http://www.w3schools.com/js/js_cookies.asp)
 	getCookie: function (cname) {
-	    var name = cname + "=";
+	    var name = cname + '=';
 	    var ca = document.cookie.split(';');
-	    for(var i = 0; i <ca.length; i++) {
+	    for (var i = 0; i < ca.length; i++) {
 	        var c = ca[i];
-	        while (c.charAt(0)==' ') {
+	        while (c.charAt(0) === ' ') {
 	            c = c.substring(1);
 	        }
 	        if (c.indexOf(name) === 0) {
-	            return c.substring(name.length,c.length);
+	            return c.substring(name.length, c.length);
 	        }
 	    }
-	    return "";
+	    return '';
 	}
 
 };
