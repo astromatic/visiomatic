4e6ce3ab20e639b5374aa8d98aadd9012874cd9c
Emmanuel Bertin <bertin@iap.fr>
2022-10-21 15:21:24 -1000

    Fix access_log option.

--- start_visiomatic.py
+++ start_visiomatic.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=defs.package_description)
 
     parser.add_argument("-H", "--host", type=str, default="localhost", help="host name or IP address (default=localhost)")
-    parser.add_argument("-a", "--access_log", type=bool, default=False, help="display access log")
+    parser.add_argument("-a", "--access_log", action='store_true', default=False, help="display access log")
     parser.add_argument("-p", "--port", type=int, default=8009, help="port (default=8009)")
     parser.add_argument("-r", "--root_path", type=str, default="", help="ASGI root_path")
     parser.add_argument("-w", "--workers", type=int, default=4, help="number of workers")
 
     args = vars(parser.parse_args())
-
+    print(args["access_log"])
     start_server(
         host=args["host"],
         port=args["port"],
