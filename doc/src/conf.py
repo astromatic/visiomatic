ca25d9a9b976a406777eef9dcf6c75273a06294a
Emmanuel Bertin <bertin@iap.fr>
2022-11-06 01:21:44 -1000

    Got it right this time.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(1, os.path.abspath('../../src/visiomatic'))
+sys.path.insert(1, os.path.abspath('../../src'))
 import sphinx_rtd_theme
 import adsarxiv
 
