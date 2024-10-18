332c972e70c07b2bf4015332d22510f22eea39eb
Emmanuel Bertin <bertin@iap.fr>
2024-10-18 22:10:29 +0200

    Add more tests.

---
+++ tests/test_quantity.py
@@ -0,0 +1,72 @@
+"""
+Quantity module tests
+"""
+# Copyright CFHT/CNRS/CEA/UParisSaclay
+# Licensed under the MIT licence
+
+from typing import Annotated
+
+from astropy import units as u
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+import pytest
+
+from visiomatic.server.config import quantity
+
+def test_QuantityAnnotation():
+    """
+    Test Quantity annotations
+    """
+    class Coordinates(BaseModel): 
+        lat: Annotated[ 
+            u.Quantity, quantity.QuantityAnnotation(
+                "deg",
+                ge=-90.*u.deg,
+                le=90.*u.deg
+            ) 
+        ] 
+        lon: Annotated[u.Quantity, quantity.QuantityAnnotation("deg")] 
+        alt: Annotated[u.Quantity, quantity.QuantityAnnotation("km")] 
+    # The following instantiation should validate 
+    coord = Coordinates(lat="39.905 deg", lon="-75.166 deg", alt="12 m")
+    assert coord
+    assert coord.model_dump()
+    assert coord.model_dump(mode="json")
+    assert coord.model_dump_json()
+    # The following instantiations should NOT validate 
+    with pytest.raises(Exception):
+        coord = Coordinates(lat="99.905 deg", lon="-75.166 deg", alt="12 m")
+    with pytest.raises(Exception):
+        coord = Coordinates(lat="-99.905 deg", lon="-75.166 deg", alt="12 m")
+    with pytest.raises(Exception):
+        coord = Coordinates(lat="-99.905 deg", lon="-75.166 deg", alt="12")
+    # The following instantiation using dictionaries should validate
+    assert Coordinates(
+        lat={'value': 39.905, 'unit': "deg"},
+        lon={'value': -75.166, 'unit': "deg"},
+        alt={'value': 12., 'unit': "m"}
+    )
+        
+
+def test_AnnotatedQuantity():
+    """
+    Test annotated quantity pseudo Pydantic-field
+    """
+    class Settings(BaseSettings): 
+        size: quantity.AnnotatedQuantity( 
+            short='S', 
+            description="an arbitrary length", 
+            default=10. * u.m, 
+            gt=1. * u.micron, 
+            lt=1. * u.km 
+        ) 
+    # The following instantiation should validate 
+    assert Settings(size="3. cm")
+    # The following instantiations should NOT validate 
+    with pytest.raises(Exception):
+        s = Settings(size="4 deg")
+    with pytest.raises(Exception):
+        s = Settings(size="0.001 mm")
+    with pytest.raises(Exception):
+        s = Settings(size="1. au")
+
